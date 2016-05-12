95fc5609a8f0c6b54917e8a8e8dce91a2e41c41a
Annika Laumets <laumets.annika@gmail.com>
2016-05-12 13:13:31 +0300

    Configuration files for one RPi setup.

---
+++ One Raspberry Pi/var/www/vhosts/app2/venv/bin/pilfile.py
@@ -0,0 +1,4 @@
+#!/var/www/vhosts/app2/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'Pillow==1.7.8','pilfile.py'
+__requires__ = 'Pillow==1.7.8'
+__import__('pkg_resources').run_script('Pillow==1.7.8', 'pilfile.py')
